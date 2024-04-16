2c16e7651c38cbf555904835db41629ed18d4aa2
Mayank-Raj3 <guptamannu884@gmail.com>
2024-04-16 23:19:36 +0530

    Made Product card

--- src/component/Products.js
+++ src/component/Products.js
@@ -2,19 +2,29 @@ import React from "react";
 import "../styles/products.css";
 import productData from "../utils/data.json";
 
+function ProductItem({ id, name, url_id }) {
+  return (
+    <div key={id} className="product">
+      <img
+        src={`https://assets.leetcode.com/users/images/${url_id}`}
+        alt={name}
+      />
+      <div className="product-name">{name}</div>
+      <button className="yellow-button">Add to Cart </button>
+    </div>
+  );
+}
+
 export function Products() {
-  console.log(productData);
   return (
     <div className="products-container">
       {productData.map((item) => (
-        <div key={item.id} className="product">
-          <img
-            src={`https://assets.leetcode.com/users/images/${item.url_id}`}
-            alt={item.name}
-          />
-          <div className="product-name">{item.name}</div>
-          <button className="yellow-button">Add to Cart </button>
-        </div>
+        <ProductItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          url_id={item.url_id}
+        />
       ))}
     </div>
   );
